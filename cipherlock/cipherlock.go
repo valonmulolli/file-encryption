784ecae702ff46f0d69746749ba39a21f74c2978
valonmulolli <valonmulolli@gmail.com>
2023-10-27 20:55:46 +0200

    abject-quartz

--- cipherlock/cipherlock.go
+++ cipherlock/cipherlock.go
@@ -1,7 +1,111 @@
-package main
+package cipherlock
 
-import "fmt"
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha1"
+	"encoding/hex"
+	"io"
+	"io/ioutil"
+	"os"
 
-func main() {
-	fmt.Println("cipherlock encrypted")
+	"golang.org/x/crypto/pbkdf2"
+)
+
+func Encrypt (source string, password []byte) {
+	if _, err := os.Stat(source); os.IsNotExist(err) {
+		panic(err.Error())
+	}
+
+	srcFile, err := os.Open(source)
+	if err != nil {
+		panic (err.Error())
+	}
+
+	defer srcFile.Close()
+
+	plaintext, err := io.ReadAll(srcFile)
+	if err != nil {
+		panic (err.Error())
+	}
+	key := password
+
+	nonce := make([]byte, 12)
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		panic (err.Error())
+	}
+
+	dk := pbkdf2.Key(key, nonce, 4096, 32, sha1.New)
+
+	block, err := aes.NewCipher(dk)
+	if err != nil {
+		panic (err.Error())
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic (err.Error())
+	}
+
+	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
+	ciphertext = append(ciphertext, nonce...)
+
+	dstFile, err := os.Create(source)
+	if err != nil {
+		panic (err.Error())
+	}
+	defer dstFile.Close()
+
+	_, err = dstFile.Write(ciphertext)
+	if err != nil {
+		panic (err.Error())
+	}
+
+} 
+
+func Decrypt(source string, password []byte) {
+
+	if _, err := os.Stat(source); os.IsNotExist(err) {
+		panic(err.Error())
+	}
+
+	ciphertext, err := ioutil.ReadFile(source)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	key := password
+	salt := ciphertext[len(ciphertext)-12:]
+	str := hex.EncodeToString(salt)
+
+	nonce, err := hex.DecodeString(str)
+
+	dk := pbkdf2.Key(key, nonce, 4096, 32, sha1.New)
+
+	block, err := aes.NewCipher(dk)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	plaintext, err := aesgcm.Open(nil, nonce, ciphertext[:len(ciphertext)-12], nil)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	f, err := os.Create(source)
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = io.Copy(f, bytes.NewReader(plaintext))
+	if err != nil {
+		panic(err.Error())
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/valonmulolli/file-encryption
 
 go 1.20
+
+require golang.org/x/crypto v0.14.0
+
+require (
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/term v0.13.0 // indirect
+)
